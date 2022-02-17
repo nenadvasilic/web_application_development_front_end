4177238ebc8741ee78574937f8a3891c060a8562
Nenad Vasilic <vasilic2013@gmail.com>
2022-02-17 13:30:47 +0100

    Project cleanup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Home
+    </div>
+  );
+}
+
+export default App;
+
+// yarn start - da bismo videli našu aplikaciju!
